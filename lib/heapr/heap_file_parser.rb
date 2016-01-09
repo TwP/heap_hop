4d29c5f35be76c0971a3cd3c0b0953f0bd689219
Tim Pease <tim.pease@gmail.com>
2016-01-09 09:03:49 -0700

    Initial parser commit
    Setting up the test framework and the initial heap file parser commit.

---
+++ lib/heapr/heap_file_parser.rb
@@ -0,0 +1,13 @@
+module Heapr
+  class HeapFileParser
+
+    attr_reader :filename
+
+    def initialize( filename )
+    end
+
+    def each
+    end
+
+  end
+end
---
+++ test/heapr/test_heap_file_parser.rb
@@ -0,0 +1,5 @@
+require File.expand_path("../../test_helper", __FILE__)
+
+describe Heapr::HeapFileParser do
+  test "not yet implemented"
+end
