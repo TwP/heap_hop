354c8326029426c938c8a2d0e80cf0f710a54e0a
Tim Pease <tim.pease@gmail.com>
2016-01-10 15:17:17 -0700

    documenting methods

--- lib/heap_hop.rb
+++ lib/heap_hop.rb
@@ -7,11 +7,19 @@ module HeapHop
   PATH = File.expand_path("../..", __FILE__).freeze
   LIBPATH = File.join(PATH, "lib").freeze
 
+  # Returns the path for HeapHop. If any arguments are given,
+  # they will be joined to the end of the path using `File.join`.
+  #
+  # Returns a String.
   def self.path( *args )
     return PATH if args.empty?
     File.join(PATH, *args)
   end
 
+  # Returns the library path for HeapHop. If any arguments are given,
+  # they will be joined to the end of the library path using `File.join`.
+  #
+  # Returns a String.
   def self.libpath( *args )
     return LIBPATH if args.empty?
     File.join(LIBPATH, *args)
@@ -23,4 +31,3 @@ require "heap_hop/heap_file_parser"
 require "heap_hop/object_store"
 require "heap_hop/version"
 require "heap_hop/reports"
-
