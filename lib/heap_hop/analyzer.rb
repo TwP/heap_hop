f1b185ce478ef3df5de40b96beeef68b91a53afc
Tim Pease <tim.pease@gmail.com>
2016-01-10 19:29:46 -0700

    mroe documentation

--- lib/heap_hop/analyzer.rb
+++ lib/heap_hop/analyzer.rb
@@ -26,8 +26,7 @@ module HeapHop
       end
     end
 
-    #
-    #
+    # Returns the name of the SQLite DB file as a String.
     def db_filename
       return @db_filename unless @db_filename.nil?
 
@@ -35,8 +34,12 @@ module HeapHop
       @db_filename = File.join(path.dirname, path.basename(path.extname)) + ".sqlite3"
     end
 
+    # Public: Parse the heap file and load all the heap objects and their
+    # references into the SQLite DB.
     #
+    # force - A boolean flag used to force loading data into the SQLite DB.
     #
+    # Returns this analyzer instance.
     def load_object_store( force: false )
       object_store.purge! if force
 
@@ -47,8 +50,11 @@ module HeapHop
       self
     end
 
+    # Public: Determine if the SQLite DB is up-to-date with the heap file data.
+    # So if the heap file has been modified after the SQLite DB file, then it is
+    # not up to date.
     #
-    #
+    # Returns `true` or `false`.
     def up_to_date?
       heap_time = heap_file_exists? ? File.mtime(heap_filename) : nil
       db_time   = db_file_exists?   ? File.mtime(db_filename)   : nil
@@ -72,21 +78,22 @@ module HeapHop
       db_filename && File.file?(db_filename)
     end
 
-    # Internal:
+    # Internal: Accessor the heap file parser. A new parser is returned each
+    # time this method is called.
     #
     # Returns the HeapFileParser
     def parser
       HeapHop::HeapFileParser.new(heap_filename)
     end
 
-    # Internal:
+    # Internal: Accessor for the object store.
     #
     # Returns the ObjectStore.
     def object_store
       @object_store ||= HeapHop::ObjectStore.new(db_filename)
     end
 
-    # Internal:
+    # Internal: Accessor the SQLite DB handle.
     #
     # Returns the SQLite database.
     def db
